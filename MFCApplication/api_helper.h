6fe004de17e5821969ef2758e9b940faa5487abe
chinna0917 <chinna3915@gmail.com>
2021-09-14 19:08:35 +0530

    2nd commit

--- MFCApplication/api_helper.h
+++ MFCApplication/api_helper.h
@@ -14,52 +14,7 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 	return size * nmemb;
 }
 
-static void save_to_file(std::string apiURL,std::string id,std::string &path)
-{
 
-
-	/*static std::ofstream file(id + ".jpg", std::ios::binary);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, reinterpret_cast<void*>(&file));*/
-	CURL *curl;
-	FILE *fp = fopen("D:\\Projects\\citi_assesment\\info.jpg", "wb");
-	CURLcode res;
-	//char *url = "http://localhost/aaa.txt";
-	////std::string pa="D:\\Projects\\citi_assesment\\info.jpg";// = /*("res/") + */id + ".jpg";
-	////const int n = pa.length();
-	////path = pa;
-	////// declaring character array
-	////char char_array[100];
-
-	// copying the contents of the
-	// string to char array
-	//strcpy(char_array, pa.c_str());
-	//char outfilename[FILENAME_MAX] = (char)pa;
-	curl = curl_easy_init();
-	//struct curl_slist *header = NULL;
-	//header = curl_slist_append(header, "Content-Type: image/jpeg");
-	//header = curl_slist_append(header, "Accept: image/jpeg");
-	if (curl)
-	{
-		//fp = fopen(char_array, "wb");
-		curl_easy_setopt(curl, CURLOPT_URL, apiURL.c_str());
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-		
-		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		fclose(fp);
-	}
-	//return "";
-} 
-//
-
-
-
-
-
-//
 static std::string GetApiData(std::string apiURL)
 {
 	CURL *curl;
